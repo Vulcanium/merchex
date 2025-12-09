8258023adc95b255e2ef530a2cb70f9f27fda681
DESKTOP-VU9GHEL\trubl <trublereau@msn.com>
2025-12-09 20:54:59 +0100

    Add HTML templates to link views and models (MVT) + Enable dynamic display of bands and listings

--- listings/views.py
+++ listings/views.py
@@ -5,30 +5,14 @@ from listings.models import Band, Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello World!</h1>
-        <p>My favorite bands are:<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-        """)
-
-def about(request):
-    return HttpResponse("<h1>About</h1> <p>We love merch!</p>")
-
-def contact(request):
-    return HttpResponse("<h1>Contact us</h1> <p>Email: test@email.com</p>")
+    return render(request, "listings/hello.html", {"bands": bands})
 
 def listings(request):
     listings = Listing.objects.all()
-    return HttpResponse(f"""
-        <h1>Listings:</h1>
-        <ul>
-            <li>{listings[0].title}</li>
-            <li>{listings[1].title}</li>
-            <li>{listings[2].title}</li>
-            <li>{listings[3].title}</li>
-        </ul>
-        """)
+    return render(request, "listings/listings.html", {"listings": listings})
+
+def about(request):
+    return render(request, "listings/about.html")
+
+def contact(request):
+    return render(request, "listings/contact.html")
