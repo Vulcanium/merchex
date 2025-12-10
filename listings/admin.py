087dd8b0b8fbaa5d8e3863909bddb75d2612aea7
DESKTOP-VU9GHEL\trubl <trublereau@msn.com>
2025-12-10 16:29:12 +0100

    Add extra fields to the Band and Listings models + Add their management in the Django Admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band, Listing
+
+# Class for displaying the fields of the Band model directly in the list of objects in the Django Admin interface
+class BandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'year_formed', 'genre']
+
+
+# Class for displaying the fields of the Listing model directly in the list of objects in the Django Admin interface
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sold', 'type']
+
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
--- listings/models.py
+++ listings/models.py
@@ -1,7 +1,34 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import datetime
+
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        ALTERNATIVE_ROCK = 'AR'
+        HEAVY_METAL = 'HM'
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(datetime.now().year)])
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
 
 class Listing(models.Model):
+
+    class Type(models.TextChoices):
+        RECORDS = "R"
+        CLOTHING = "C"
+        POSTERS = "P"
+        MISCELLANEOUS = "M"
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(null=True, blank=True, validators=[MinValueValidator(1900), MaxValueValidator(datetime.now().year)])
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
